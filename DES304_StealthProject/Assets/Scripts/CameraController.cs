86ef310893855a0c57343a46051bc3d5a53ff408
Gaz Robinson <5627386+GazRobinson@users.noreply.github.com>
2021-10-13 14:30:09 +0100

    Movement + camera controller

---
+++ DES304_StealthProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+
+    private float yaw = 0.0f, pitch = 0.0f;
+    private float m_Distance = 5.0f;
+
+    public Vector2 m_RotationSpeed = Vector2.one;
+
+    public void Rotate(Vector2 direction ) {
+        yaw += direction.x * Time.deltaTime * m_RotationSpeed.x;
+        pitch += direction.y * Time.deltaTime * m_RotationSpeed.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (m_Target) {
+            transform.position = m_Target.position;
+            transform.rotation = Quaternion.AngleAxis( yaw, Vector3.up ) * Quaternion.AngleAxis( pitch, Vector3.right );
+            transform.position -= transform.forward * m_Distance;
+        }
+    }
+
+    public Vector3 GetForward() {
+        Vector3 fwd = transform.forward;
+        fwd.y = 0.0f;
+        return fwd.normalized;
+    }
+    public Vector3 GetRight() {
+        Vector3 right = transform.right;
+        right.y = 0.0f;
+        return right.normalized;
+    }
+}
--- DES304_StealthProject/Assets/Scripts/PlayerController.cs
+++ DES304_StealthProject/Assets/Scripts/PlayerController.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class PlayerController : Actor
 {
-    Vector2 inputDirection;
-    bool jump = false;
+    [SerializeField]
+    private CameraController m_Camera;
+    private Vector2 inputDirection;
+    private bool jump = false;
     void GetInput() {
         inputDirection = new Vector2( Input.GetAxis( "Horizontal" ), Input.GetAxis( "Vertical" ) );
         if (Input.GetButtonDown( "Jump" )) {
             jump = true;
         }
+
+        m_Camera.Rotate(new Vector2(Input.GetAxis( "CameraHorizontal" ), Input.GetAxis( "CameraVertical" ) ) );
     }
 
     // Update is called once per frame
     protected override void OnUpdate()
     {
         GetInput();
-
-        velocity.x = inputDirection.x * m_Stats.m_GroundSpeed;
-        velocity.z = inputDirection.y * m_Stats.m_GroundSpeed;
+        Vector3 movement = ( inputDirection.x * m_Camera.GetRight() ) + ( inputDirection.y * m_Camera.GetForward() );
+        movement.Normalize();
+        velocity.x = movement.x;
+        velocity.z = movement.z;
+        velocity *= m_Stats.m_GroundSpeed;
 
         if(jump && m_IsGrounded) {
             velocity.y = 5.0f;
             jump = false;
         }
+        transform.LookAt( transform.position + movement );
     }
 }
